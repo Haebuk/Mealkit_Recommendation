f3c52a8f56196bc4df3df5fdb86bd273e5d052f9
Haebuk <cwryu6252@naver.com>
2021-09-26 20:51:14 +0900

    이마트 리뷰 스크래퍼 완성

--- emart_mall/emart_scrapping.py
+++ emart_mall/emart_scrapping.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebElement
 
 class Emart_Scrapping(webdriver.Chrome):
-    def __init__(self, driver_path = 'C:/Program Files (x86)/Google./chromedriver', teardown=False): # C 드라이브에 있는 크롬 드라이버를 사용하도록 설정
+    def __init__(self, driver_path = 'C:/chromedriver.exe', teardown=False): # C 드라이브에 있는 크롬 드라이버를 사용하도록 설정
         self.teardown = teardown
         options = webdriver.ChromeOptions()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -37,8 +37,8 @@ class Emart_Scrapping(webdriver.Chrome):
     def get_product_name(self): # 상품의 이름을 가져오는 함수
         product_name = self.find_element_by_class_name(
             'cdtl_prd_info'
-        ).find_element_by_class_name('cdtl_info_tit').text.split('\n')
-        print(f'product name: {product_name[0]}')
+        ).find_element_by_class_name('cdtl_info_tit').text.split('\n')[0]
+        print(f'product name: {product_name}')
         return product_name
     
     def get_product_brand(self): # 상품의 브랜드를 가져오는 함수
@@ -58,19 +58,6 @@ class Emart_Scrapping(webdriver.Chrome):
             brand = product_name[0][1:idx]
         print(f'product brand: {brand}')
         return brand
-    
-    def get_product_information(self): # 상품 정보를 가져오는 함수
-        information = self.find_element_by_class_name('tmpl_sub_tit').text
-        print(f'product information: {information}')
-        return information
-
-    def get_product_information(self): # 상품 정보를 가져오는 함수
-        self.switch_to.frame("_ifr_html") # iframe 
-        product_information = self.find_element_by_class_name(
-            'tmpl_sub_tit'
-        ).text
-        print(f'product information: {product_information}')
-        return product_information
 
     def get_soldout_info(self): # 품절 여부 판단하는 함수
         if self.find_element_by_class_name(
@@ -114,6 +101,48 @@ class Emart_Scrapping(webdriver.Chrome):
     def get_product_price(self): # 상품의 가격을 가져오는 함수
         product_price = self.find_element_by_class_name(
             'cdtl_info_wrap'
-        ).find_element_by_class_name('ssg_price').text.split('\n')
-        print(f'product price: {product_price[0]}')
+        ).find_element_by_class_name('ssg_price').text.split('\n')[0]
+        print(f'product price: {product_price}')
         return product_price
+
+    def get_review_info(self): # 상품의 리뷰 정보를 가져오는 함수
+        """
+        return: reviews, starts, users
+        """
+        button_box = self.find_element_by_id('comment_navi_area') # 버튼박스
+        reviews = [] # 리뷰를 저장할 리스트
+        stars = [] # 별점을 저장할 리스트
+        users = [] # 유저명을 저장할 리스트
+        try:
+            for page in range(2, 7):
+                print(f'review page: {page-1}')
+                review_box = self.find_element_by_id('cdtl_cmt_tbody') # 리뷰 컨테이너
+                user_list = review_box.find_elements_by_class_name('user') # 유저명
+                star_list = review_box.find_elements_by_class_name('star') # 평점
+                desc_txt = review_box.find_elements_by_class_name('desc_txt') # 리뷰. desc 바로 잡으면 10개가 더잡히기 때문에 한번 더 찾아줌
+
+                for i, (user, star, desc) in enumerate(zip(user_list, star_list, desc_txt)): # 리뷰 저장
+                    desc = desc.text
+                    if desc.split('\n')[0] == '사진' or desc.split('\n')[0] == '비디오':
+                        desc = desc.split('\n')[1:]
+                    if desc[0] == '사진' or desc[0] == '비디오':
+                        desc = desc[1:]
+                    users.append(user.text)
+                    stars.append(star.text.split(' ')[-1][0])        
+                    reviews.append(''.join(desc))
+
+                time.sleep(1)
+                button_box.find_element_by_link_text(str(page)).click()
+                time.sleep(1)
+        except Exception as e:
+            print(e)
+            print('리뷰가 부족합니다.')
+            return reviews, stars, users
+        return reviews, stars, users
+
+    def get_product_category(self): # 카테고리 가져오는 함수
+        product_category = self.find_element_by_id(
+            'area_disp_ctg_title'
+            ).find_element_by_tag_name('a').text.strip()
+        print(f'product category: {product_category}')
+        return product_category
--- emart_run.py
+++ emart_run.py
@@ -1,7 +1,7 @@
 import time
-from tqdm import tqdm
-from random import randint
 from emart_mall.emart_scrapping import Emart_Scrapping
+from utils.data_to_json import DataToJson
+from utils.refine_information import RefineInformation
 """
 이마트몰 크롤링 실행 파일
 마켓컬리는 &page= 쿼리 스트링으로 url이 되어 있기 때문에 페이지를 입력해 자동화가 가능
@@ -15,12 +15,21 @@ try:
         
         input1 = int(input("시작할 페이지: ")) # 시작할 페이지
         input2 = int(input("종료할 페이지: ")) # 종료할 페이지
+        filename = f'이마트몰_{input1}_{input2}.json'
+        review_filename = f'이마트몰리뷰_{input1}_{input2}.json'
+        data = DataToJson(filename)
+        review_data = DataToJson(review_filename)
+
         for page in range(input1, input2+1):
+            json_data = data.load_json()
+            json_review = review_data.load_json()
             emart.land_first_page(page)
+            product_category = emart.get_product_category()
             product_list = emart.get_product_list()
             print(f'product list length: {len(product_list)}')
             product_image_urls = emart.get_product_image_url()
             for iter in range(len(product_list)):
+                start = time.time()
                 print(f'----- page: {page}, iter: {iter+1} -----')
                 image_url = product_image_urls[iter]
                 product_url = emart.access_product(iter)
@@ -29,8 +38,34 @@ try:
                 product_brand = emart.get_product_brand() # 상품 브랜드 수집
                 product_price = emart.get_product_price()
                 is_sold_out = emart.get_soldout_info()
-                product_info = emart.get_product_information() # 상품 정보 수집
+                reviews, stars, users = emart.get_review_info()
+                product_info = emart.get_product_information() # 상품 정보 수집. 가장 마지막
+                product_dict = RefineInformation().refine_information(
+                    title=product_name,
+                    store_name='이마트몰',
+                    category=product_category,
+                    brand=product_brand,
+                    price=product_price,
+                    img_url=image_url,
+                    product_url=product_url,
+                    is_sold_out=is_sold_out,
+                    detail=product_info
+                )
+                review_dict = RefineInformation().refine_review(
+                    title=product_name,
+                    users=users,
+                    stars=stars,
+                    contents=reviews
+                )
+                json_data.append(product_dict)
+                json_review.append(review_dict)
+
+                data.save_json(json_data)
+                review_data.save_json(json_review)
+                end = time.time()
                 emart.land_first_page(page)
+
+
 except Exception as e:
     """
     처음 실행시 셀레니움 경로 관련 오류가 발생할 수 있는데,
